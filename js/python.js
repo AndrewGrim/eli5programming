ae062c774bce39a4499aefd70007f3d4bb01e5b5
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-03-21 15:47:07 +0000

    Reworked the python page and renamed css file...
    Previously it was difficult to work with the template,
      which made things more complex for no benefit.
    The new version simplifies everything.
    Removed any unused css.
    Changed naming in the css file to be more relevant.
    Added documentation comments where applicable.
    Each video/playlist can now be hidden/shown by pressing a button.
    Added an extra playlist for a total of two.

--- js/python.js
+++ js/python.js
@@ -1,4 +1,13 @@
-function displayContent(elementID) {
+/*
+Displays or hides the passed in "elementID".
+
+args:
+    elementID: String = The string id of the element to be hidden/shown.
+
+returns:
+    None
+*/
+function event_expand(elementID) {
     var element = document.getElementById(elementID);
     if (element.style.display === "inline-block" || element.style.display === "") {
         element.style.display = "none";
