55ead0abebb2a83acee41fa329d6ee1b90c255ba
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-03-29 18:29:09 +0100

    Reworked the index page to fit the current design

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function show_more() {
+    var element = document.getElementById("more");
+    element.remove()
+    element = document.getElementById("more-text");
+    element.style.display = "inline";
+}
