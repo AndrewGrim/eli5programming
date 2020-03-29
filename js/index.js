fd67886fc1848e38170885900462c4ad71bf62d4
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-03-29 19:22:40 +0100

    Added more doc comments, changed font weight

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+/*
+Removes the "Read More" button and reveals more text.
+
+args:
+    None
+
+returns:
+    None
+*/
 function show_more() {
     var element = document.getElementById("more");
     element.remove()
